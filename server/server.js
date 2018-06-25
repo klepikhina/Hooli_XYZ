b75a826364285b0c419884470468943d2b45ef97
LukeMyers <wade.myers@colorado.edu>
2018-06-25 14:47:14 -0600

    mistakes were fixed

---
+++ server/server.js
@@ -0,0 +1,28 @@
+//NODE MODULES
+const express               = require("express")
+const expressValidator      = require("express-validator")
+const mysql                 = require("mysql")
+const bodyParser            = require("body-parser")
+const fs                    = require("fs")
+const path                  = require("path")
+const ejs                   = require("ejs")
+//FILES
+const config                = require("./config")
+//APP
+let app = express()
+let router = express.Router()
+
+//SET
+app.set('views', path.join(__dirname, '/../client/views'))
+console.log(path.join(__dirname, '/../client/views'))
+app.set('view engine', 'html')
+//USE
+app.use(router)
+//ROUTER
+router.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, '/../client/views/index.html'))
+})
+//START
+app.listen(config.server.port, function(){
+  console.log("started on", config.server.port)
+})
