5591342a72347a1e86f716dc979495aa46df9606
Ksenia Lepikhina <kklepi@gmail.com>
2018-06-28 21:49:25 -0600

    fixed

--- server/server.js
+++ server/server.js
@@ -6,11 +6,8 @@ const bodyParser            = require("body-parser")
 const fs                    = require("fs")
 const path                  = require("path")
 const ejs                   = require("ejs")
-const mocha                 = require("mocha")
-const chai                  = require("chai")
 //CONFIG
 const config                = require("./config")
-<<<<<<< HEAD
 //APP
 var app = express()
 var router = express.Router()
@@ -25,24 +22,10 @@ app.use("/css", express.static(path.join(__dirname, '/../client/public/css')))
 app.use("/img", express.static(path.join(__dirname, '/../client/public/img')))
 app.use(router)
 //ROUTER
-=======
-
-
-let app = express()
-let router = express.Router()
-
->>>>>>> 2b2ad5274a6df0b0982bd17e23a7593c83824968
 router.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/../client/views/loginpage.html'))
-  res.send("Good Response")
 })
-
-app.set('views', path.join(__dirname, '/../client/views'))
-app.use(express.static(path.join(__dirname, '/../client/public')))
-app.use("/css", express.static(path.join(__dirname, '/../client/public/css')))
-app.use(router)
-
-app.listen(config.server.port, function(err){
-  if(err){console.log(err)}
+//START
+app.listen(config.server.port, function(){
   console.log("started on", config.server.port)
 })
